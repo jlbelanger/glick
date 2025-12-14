33549a48260cc8f294f1c41cab4757b9b08ceeed
Jenny Belanger <jbelangr@gmail.com>
2025-12-14 17:07:52 -0500

    Fix mixins

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
+import postcssMixins from 'postcss-mixins';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(() => ({
 	build: {
 		outDir: 'build',
+	},
+	css: {
+		postcss: {
+			plugins: [
+				postcssMixins,
+			],
+		},
 	},
 	plugins: [
 		react(),
